b198b063c4a8c3ab3ddc9fa464aa210de24f83a2
Bruce WL Huang <brucewl_huang@asus.com>
2022-08-25 16:42:29 +0800

    fix(test_calc_func.py): add another two test cases for add method

--- week2/py_calculator/tests/test_calc_func.py
+++ week2/py_calculator/tests/test_calc_func.py
@@ -21,6 +21,18 @@ class TestClass:
         result = self.cal_func.add(num1, num2)
         assert result != 5.0
 
+    def test_add_expect_result_bigger_than_zero(self):
+        num1 = 3.0
+        num2 = -1.0
+        result = self.cal_func.add(num1, num2)
+        assert result > 0.0
+
+    def test_add_expect_result_smaller_than_zero(self):
+        num1 = 3.0
+        num2 = -5.0
+        result = self.cal_func.add(num1, num2)
+        assert result < 0.0
+
 
 if __name__ == '__main__':
     pytest.main()

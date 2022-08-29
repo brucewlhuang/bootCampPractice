04ae2989ac1d6437d31de5a8653b493cd2c28c36
Bruce WL Huang <brucewl_huang@asus.com>
2022-08-29 11:38:40 +0800

    feat(calculator.js): new multiply method

--- week2/js_calculator/src/calculator.js
+++ week2/js_calculator/src/calculator.js
@@ -12,7 +12,15 @@ function subtract(numbers) {
       .reduce((a, b) => a - b);
 }
 
+function multiply(numbers) {
+  return numbers
+      .split(',')
+      .map((x) => parseInt(x))
+      .reduce((a, b) => a * b);
+}
+
 module.exports = {
   add,
   subtract,
+  multiply,
 };
